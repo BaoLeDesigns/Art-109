9380be8a8effcf60fa03d77c5d337643e7ad3223
BaoLeDesigns <91010332+BaoLeDesigns@users.noreply.github.com>
2025-03-05 01:17:31 -0800

    removed mix-blend as switched to brightness approach + moved const to outside for better practice

--- Project 2/js/main.js
+++ Project 2/js/main.js
@@ -1,6 +1,6 @@
-document.addEventListener("mousemove", function(event) {
-    const flashlight = document.querySelector(".flashlight");
+const flashlight = document.querySelector(".flashlight");
 
+document.addEventListener("mousemove", function(event) {
     flashlight.style.left = `${event.clientX}px`;
     flashlight.style.top = `${event.clientY}px`;
 });
