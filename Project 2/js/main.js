cbd3ec500768e3ce8f20d876cec4a49525583eca
BaoLeDesigns <91010332+BaoLeDesigns@users.noreply.github.com>
2025-03-06 00:19:58 -0800

    Added alerts to use as text for story + another image
    BUG TO FIGURE OUT IN FUTURE: only window.onload and first image show up alerts when clicked, other 2 don't.

--- Project 2/js/main.js
+++ Project 2/js/main.js
@@ -4,3 +4,26 @@ document.addEventListener("mousemove", function (event) {
     flashlight.style.left = `${event.clientX}px`;
     flashlight.style.top = `${event.clientY}px`;
 });
+
+
+// alert box messages for story
+
+window.onload = function() {
+    if (!sessionStorage.getItem("alertShown")) {
+        alert("hi");
+        sessionStorage.setItem("alertShown", "true");
+}}
+
+document.querySelector(".image-1").addEventListener("click", function() {  
+    alert("You see my gaze, but not my true form, look into a mirror first before I will speak to you fully."); 
+});
+
+document.querySelector(".image-2").addEventListener("click", function() {  
+    alert("now you can find my true face, but you must find where I am."); 
+    document.querySelector(".image-1").style.display = "none";
+});
+
+document.querySelector(".image-3").addEventListener("click", function() {  
+    alert("test test"); 
+});
+
