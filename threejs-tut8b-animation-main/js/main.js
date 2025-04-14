ea3d0836efeab5d04f4655a15fc0ed3b8dccfb95
BaoLeDesigns <91010332+BaoLeDesigns@users.noreply.github.com>
2025-04-14 02:52:36 -0700

    loaded animation + set up for mouse event

--- threejs-tut8b-animation-main/js/main.js
+++ threejs-tut8b-animation-main/js/main.js
@@ -5,7 +5,7 @@ glTF import:
 -Two directional lights added to view glTF
 - Added HELPERS to debug light position (disable after you place them)
 -glTF imported from blender (not it is an *embedded* .glTF file, not .glb)
--Changed material on cube from BASIC to STANDARD so that the geometry catches light
+-Changed material on ball from BASIC to STANDARD so that the geometry catches light
 */
 
 
@@ -20,7 +20,7 @@ import { GLTFLoader } from 'https://unpkg.com/three@0.162.0/examples/jsm/loaders
 
 
 // ~~~~~~~~~~~~~~~~ Declare Global Variables~~~~~~~~~~~~~~~~
-let scene, camera, renderer, cube, dog;
+let scene, camera, renderer, ball, dog, mixer;
 
 
 // ~~~~~~~~~~~~~~~~ Initialize Scene in init() ~~~~~~~~~~~~~~~~
@@ -29,6 +29,8 @@ function init() {
     // ~~~~~~Set up scene, camera, + renderer ~~~~~~
 
     scene = new THREE.Scene();
+scene.background = new THREE.Color(0x0250281)
+
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -70,29 +72,40 @@ function init() {
 
     // ~~~~~~ Create Geometry ~~~~~~
 
-    // ---> create cube
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    // ---> create ball
+    const geometry = new THREE.SphereGeometry(.2, 32, 16);
 
     // -> change material from Basic to standard for geometry to capture lights
     // const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 
-    const texture = new THREE.TextureLoader().load('textures/animalPrint-crop-512.png');
+    const texture = new THREE.TextureLoader().load('textures/grasslight-big.jpg');
 
     const material = new THREE.MeshStandardMaterial({ map: texture });
     // texture.minFilter = THREE.LinearFilter; // makes image sharper but aliased
 
-    cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    ball = new THREE.Mesh(geometry, material);
+    scene.add(ball);
 
 
     // --> Load glTF
 
     // load dog model
+
+
     loader.load('assets/dog_shiny.gltf', function (gltf) {
         dog = gltf.scene;
         scene.add(dog);
         dog.scale.set(2, 2, 2); // scale your model
         dog.position.y = -2; // set initial position
+        mixer = new THREE.AnimationMixer(dog);
+        const clips = gltf.animations;
+        //const clip = THREE.AnimationClip.findByName(clips, 'tail')
+        //const action = mixer.clipAction(clip);
+        //action.play();
+        clips.forEach(function(clip){
+            const action = mixer.clipAction(clip);
+            action.play();
+        });
     });
 
 
@@ -108,18 +121,28 @@ function init() {
 
 // ~~~~~~~~~~~~~~~~ Animation Loop ~~~~~~~~~~~~~~~~
 // (similar to draw loop in p5.js, updates every frame)
+const clock = new THREE.Clock();
 
 function animate() {
     requestAnimationFrame(animate); // start loop by with frame update
-
+    if(mixer)
+    mixer.update(clock.getDelta());
     // →→→→→→ add your animation here ↓↓↓↓
 
-    cube.rotation.x += 0.007;
-    cube.rotation.y += 0.007;
+
+    ball.rotation.x += 0.007;
+    ball.rotation.y += 0.007;
+
+
+    ball.position.x = Math.sin(Date.now()/3000) * 3;
+    ball.position.y = Math.sin(Date.now()/1000) * 2;
+    ball.position.z = Math.sin(Date.now()/4000) * 4;
+
 
     if (dog) {
         // dog.rotation.x += 0.007;
-        dog.rotation.y += 0.007;
+        //dog.rotation.y += 0.007;
+        dog.rotation.y = Math.sin(Date.now()/500) * .5;
     }
 
 
